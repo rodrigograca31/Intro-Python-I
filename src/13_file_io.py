6e76f3d1cd5f1c42f4af7ccc0bec518777b7bd54
Rodrigo Graça <rodrigograca31@gmail.com>
2020-04-06 19:38:38 +0100

    feat: 🎸 13_file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+file1 = open("foo.txt", "r")
+contents = file1.read()
+print(contents)
+# print(file1)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+file2 = open("bar.txt", "w")
+file2.write("line 1\n")
+file2.write("line 2\n")
+file2.write("line 3\n")
+
+name = input("Whats your name? ")
+file2.write(name)
