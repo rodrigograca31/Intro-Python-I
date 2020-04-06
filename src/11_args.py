9988f627f27f6c2088667d79a1f002e54cbb8226
Rodrigo Graça <rodrigograca31@gmail.com>
2020-04-06 18:12:28 +0100

    feat: 🎸 11_args

--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,11 @@
 
 # YOUR CODE HERE
 
+
+def f1(arg1, arg2):
+    return arg1 + arg2
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -13,16 +18,22 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    sum = 0
+    for ele in args:
+        sum = sum + ele
+    return sum
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
+
+print(f2(1))  # Should print 1
+print(f2(1, 3))  # Should print 4
+print(f2(1, 4, -12))  # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,9 +41,23 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(arg1=0, arg2=None):
+    if arg2 != None:
+        return arg1 + arg2
+    else:
+        return arg1 + 1
+
+
+# v2
+def f3(*args):
+    if len(args) is 2:
+        return args[0] + args[1]
+    else:
+        return args[0] + 1
+
 
 print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+print(f3(8))  # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -44,6 +69,16 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for arg in kwargs:
+        print(f"key: {arg:s}, value: {str(kwargs[arg]):s}")
+
+
+# v2
+def f4(**kwargs):
+    for arg, value in kwargs.items():
+        print(f"key: {arg:s}, value: {str(value):s}")
+
 
 # Should print
 # key: a, value: 12
@@ -56,10 +91,7 @@ f4(a=12, b=30)
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+d = {"monster": "goblin", "hp": 3}
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
